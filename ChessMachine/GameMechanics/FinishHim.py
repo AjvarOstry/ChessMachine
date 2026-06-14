68a1552edcfb4eae6bfd5e0bbe233c84e2699cbb
AjvarOstry <another.long.nickname@gmail.com>
2026-06-14 20:06:04 +0200

    końcówki. Sorry, zapomniałem przełączyć brancha XD

--- ChessMachine/GameMechanics/FinishHim.py
+++ ChessMachine/GameMechanics/FinishHim.py
@@ -1,4 +1,43 @@
 # tablice końcówek Sygyzy 5-men
 
-def finish_him():
-    pass
+import chess
+
+class FinishHim:
+
+    def __init__(self, cat_path):
+        self.cat_path = cat_path
+
+
+    def get_move(self, board):
+
+        if(len(board.piece_map()) > 4):
+            return None
+
+        else:
+
+            top_move = None
+            top_DTZ = None
+
+            for move in board.legal_moves:
+                board.push(move)
+
+                try:
+                    WDL = self.tablebase.get_wdl(board)
+                    DTZ = self.tablebase.get_dtz(board)
+
+                    # to jest ruch z perspektywy przeciwnika,
+                    #sprawdziłem. Żebym tego nie overthinkował znowu
+                    if WDL == -2 and DTZ is not None:
+
+                        if top_DTZ is None or abs(DTZ) > abs(top_DTZ):
+                            top_DTZ = DTZ
+                            top_move = move
+
+                except:
+                    pass
+
+                board.pop()
+
+        return top_move
+
+
---
+++ ChessMachine/z_project_data/sygyzy-4-men/check_integrity.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Jorge Ruiz Centelles (c) 2025 https://ijccrl.com/
+echo.
+echo Checking the integrity of the 3-4-5 pieces Syzygy 
+echo.
+fsum -c checksum.md5
+PAUSE
