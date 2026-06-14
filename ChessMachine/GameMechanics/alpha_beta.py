5935e20b64f4239be9c28a0e49aa684741dcf6e1
wojciesze21 <iszczak.wojciech@gmail.com>
2026-06-14 04:33:03 +0200

    basic alpha_beta

---
+++ ChessMachine/GameMechanics/alpha_beta.py
@@ -0,0 +1,92 @@
+import math
+
+INF = math.inf
+
+def alpha_beta(board, depth, alpha, beta, isMax):
+
+    # do evaluate zrobię może numbą (biblioteka do kompilacji jit)
+    if depth == 0:
+        return evaluate(board)
+
+    if isMax:
+
+        value = -INF
+
+        for move in board.legal_moves:
+
+            board.push(move)
+
+            value = max(
+                value,
+                alpha_beta(
+                    board,
+                    depth - 1,
+                    alpha,
+                    beta,
+                    False
+                )
+            )
+
+            board.pop()
+
+            alpha = max(alpha, value)
+
+            if alpha >= beta:
+                break
+
+        return value
+
+    else:
+
+        value = INF
+
+        for move in board.legal_moves:
+
+            board.push(move)
+
+            value = min(
+                value,
+                alpha_beta(
+                    board,
+                    depth - 1,
+                    alpha,
+                    beta,
+                    True
+                )
+            )
+
+            board.pop()
+
+            beta = min(beta, value)
+
+            if alpha >= beta:
+                break
+
+        return value
+
+def get_best_moves(board, depth):
+
+    results = []
+
+    for move in board.legal_moves:
+
+        board.push(move)
+
+        score = alpha_beta(
+            board,
+            depth - 1,
+            -INF,
+            INF,
+            False
+        )
+
+        board.pop()
+
+        results.append((move, score))
+
+    results.sort(
+        key=lambda x: x[1],
+        reverse=True
+    )
+
+    return results
