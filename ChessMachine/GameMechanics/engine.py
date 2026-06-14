f23349ebabfc4c397b050525512ba8d11c5bc720
wojciesze21 <iszczak.wojciech@gmail.com>
2026-06-14 22:12:48 +0200

    Update engine.py

--- ChessMachine/GameMechanics/engine.py
+++ ChessMachine/GameMechanics/engine.py
@@ -8,6 +8,11 @@ from GameMechanics.open_game import open_game
 
 def engine(board):
 
+    opening_move = open_game(board, "../z_project_data/openings.bin")
+
+    if opening_move is not None:
+        return opening_move
+
     legal_moves = list(board.legal_moves)
 
     if not legal_moves:
@@ -15,39 +20,42 @@ def engine(board):
 
     best_move = random.choice(legal_moves)
 
-    time_for_round = TimeManager.give_me_time(
-        # TODO
+    time_for_round = time_manager.schedlue_time(
+        my_time_left,
+        enemy_time_left,
+        board,
+        rounds_passed
     )
-
-    opening_move = open_game(board, "../z_project_data/openings.bin")
-
-    if opening_move is not None:
-        return opening_move
 
     def find_best_move(board):
 
         nonlocal best_move
 
-        top_moves = get_best_moves(
-            board,
-            depth=3,
-            k_max=10
-        )
+        current_depth = 3
 
-        if not top_moves:
-            return
+        while True:
+            top_moves = get_best_moves(
+                board,
+                depth=current_depth,
+                k_max=1
+            )
 
-        best_move = top_moves[0][0]
+            if not top_moves:
+                return
 
-        # sekcja z modelem
+            best_move = top_moves[0][0]
+
+            current_depth += 1
+
+            # sekcja z modelem
 
     worker = threading.Thread(
         target=find_best_move,
+        args=(board,),
         daemon=True
     )
 
     worker.start()
-
     worker.join(timeout=time_for_round)
 
     return best_move
